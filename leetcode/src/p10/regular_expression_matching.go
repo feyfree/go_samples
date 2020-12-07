586bcf255e585ea082f0ca2adfa1a969148f39b1
leilei <feyfree@163.com>
2020-12-07 18:11:44 +0800

    [ADD] add leetcode 10 dp solution

--- leetcode/src/p10/regular_expression_matching.go
+++ leetcode/src/p10/regular_expression_matching.go
@@ -1,5 +1,25 @@
 package p10
 
 func isMatch(s string, p string) bool {
-	return false
+	// 动态规划 二维数组
+	m := len(s)
+	n := len(p)
+	dp := make([][]bool, m+1)
+	for i := range dp {
+		dp[i] = make([]bool, n+1)
+	}
+	dp[m][n] = true
+	for i := m; i >= 0; i-- {
+		for j := n - 1; j >= 0; j-- {
+			// i, j 的字面匹配
+			firstMatch := i < m && (s[i] == p[j] || p[j] == '.')
+			// 更新dp[i][j]
+			if j < n-1 && p[j+1] == '*' {
+				dp[i][j] = (firstMatch && (dp[i+1][j] || dp[i][j+2])) || (!firstMatch && dp[i][j+2])
+			} else {
+				dp[i][j] = firstMatch && dp[i+1][j+1]
+			}
+		}
+	}
+	return dp[0][0]
 }
