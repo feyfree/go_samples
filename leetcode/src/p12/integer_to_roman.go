e430d168a0c30c8e167e9b40345e5c8afb187470
leilei <gengll2@yunjiglobal.com>
2020-12-08 22:39:52 +0800

    [ADD] leetcode 12 solution

--- leetcode/src/p12/integer_to_roman.go
+++ leetcode/src/p12/integer_to_roman.go
@@ -1,6 +1,26 @@
 package p12
 
+import "strings"
+
 // 罗马数字
 func intToRoman(num int) string {
-	return ""
+	values := []string{
+		"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
+		"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+		"C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+		"M", "MM", "MMM"}
+	numArray := [4]int{}
+	i := 3
+	for num > 0 {
+		numArray[i] = num % 10
+		num = num / 10
+		i--
+	}
+	result := strings.Builder{}
+	for j := 0; j < 4; j++ {
+		if numArray[j] != 0 {
+			result.WriteString(values[9*(3-j)+numArray[j]-1])
+		}
+	}
+	return result.String()
 }
