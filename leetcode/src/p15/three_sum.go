878a602f375dbc399869921b6d58a25f609e1794
leilei <feyfree@163.com>
2020-12-11 15:05:49 +0800

    [REV] 代码优化 去除不必要的临时变量

--- leetcode/src/p15/three_sum.go
+++ leetcode/src/p15/three_sum.go
@@ -1,9 +1,43 @@
 package p15
 
+import "sort"
+
 //Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
 //Find all unique triplets in the array which gives the sum of zero.
 //
 //Notice that the solution set must not contain duplicate triplets.
 func threeSum(nums []int) [][]int {
-	return nil
+	n := len(nums)
+	if n < 3 {
+		return make([][]int, 0)
+	}
+	sort.Ints(nums)
+	var result [][]int
+	for i := 0; i < n; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		start := i + 1
+		end := n - 1
+		for start < end {
+			sum := nums[start] + nums[end] + nums[i]
+			if start-1 > i && nums[start] == nums[start-1] {
+				start++
+				continue
+			}
+			if sum == 0 {
+				temp := make([]int, 3)
+				temp[0] = nums[i]
+				temp[1] = nums[start]
+				temp[2] = nums[end]
+				result = append(result, temp)
+				start++
+			} else if sum > 0 {
+				end--
+			} else {
+				start++
+			}
+		}
+	}
+	return result
 }
