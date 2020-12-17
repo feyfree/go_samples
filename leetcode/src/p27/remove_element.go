6a093eb58702602e6ddbf624cda742e574d4b0e5
leilei <feyfree@163.com>
2020-12-17 09:03:18 +0800

    [REV] refactor code

--- leetcode/src/p27/remove_element.go
+++ leetcode/src/p27/remove_element.go
@@ -2,7 +2,8 @@ package p27
 
 func removeElement(nums []int, val int) int {
 	index := 0
-	// 这里的index 跟 p26 index 不同, index 最后表示的是next place index 实际上就是len
+	// 这里的index 跟 p26 index 不同,
+	// index 最后表示的是next place index 实际上就是len
 	for i := 0; i < len(nums); i++ {
 		if nums[i] != val {
 			nums[index] = nums[i]
