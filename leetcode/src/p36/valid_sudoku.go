1a2c54165cebd47d274e419417e983d0f7bff7fc
leilei <feyfree@163.com>
2020-12-22 17:52:57 +0800

    [ADD] leetcode 35, 36 solution

--- leetcode/src/p36/valid_sudoku.go
+++ leetcode/src/p36/valid_sudoku.go
@@ -1,5 +1,22 @@
 package p36
 
 func isValidSudoku(board [][]byte) bool {
-	return false
+	row := [9][9]bool{}
+	col := [9][9]bool{}
+	block := [9][9]bool{}
+	for r := 0; r < 9; r++ {
+		for c := 0; c < 9; c++ {
+			if board[r][c] != '.' {
+				value := board[r][c] - '1'
+				blockIndex := r/3*3 + c/3
+				if row[r][value] || col[c][value] || block[blockIndex][value] {
+					return false
+				}
+				row[r][value] = true
+				col[c][value] = true
+				block[blockIndex][value] = true
+			}
+		}
+	}
+	return true
 }
